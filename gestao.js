2e6b76a815abf757ecc35cf04bd76c7e979a1b72
brunoanalista7 <brunoanalista7@gmail.com>
2026-01-16 14:54:05 -0300

    mudança da logo

--- gestao.js
+++ gestao.js
@@ -1,51 +1,51 @@
 // Dados de gestão: 1 gerente + 3 supervisores por tabela
 const gestaoPorTabela = {
   A: {
-    gerente: { nome: "João Paulo", foto: "gestao1.jpeg", whatsapp: "5583988888888", cargo: "Gerente Comercial", tabela: ["Tabela A"] },
+    gerente: { nome: "Ariusca", foto: "gestao1.jpeg", whatsapp: "5583988888888", cargo: "Gerente Comercial", tabela: ["Tabela A"] },
     supervisores: [
-      { nome: "Sandra", foto: "gestao2.jpeg", whatsapp: "5583977777777", cargo: "Supervisor Comercial", tabela: ["Tabela A"] },
-      { nome: "Marcos", foto: "gestao3.jpeg", whatsapp: "5583966666666", cargo: "Supervisor Comercial", tabela: ["Tabela A"] },
-      { nome: "Paula", foto: "gestao4.jpeg", whatsapp: "5583955555555", cargo: "Supervisor Comercial", tabela: ["Tabela A"] },
+      { nome: "Supervisor1", foto: "gestao2.jpeg", whatsapp: "5583977777777", cargo: "Supervisor Comercial", tabela: ["Tabela A"] },
+      { nome: "supervisor2", foto: "gestao3.jpeg", whatsapp: "5583966666666", cargo: "Supervisor Comercial", tabela: ["Tabela A"] },
+      { nome: "supervisor3", foto: "gestao4.jpeg", whatsapp: "5583955555555", cargo: "Supervisor Comercial", tabela: ["Tabela A"] },
     ]
   },
   B: {
-    gerente: { nome: "Fernanda Costa", foto: "gestao5.jpeg", whatsapp: "5583944444444", cargo: "Gerente Comercial", tabela: ["Tabela B"] },
+    gerente: { nome: "Antonio", foto: "gestao5.jpeg", whatsapp: "5583944444444", cargo: "Gerente Comercial", tabela: ["Tabela B"] },
     supervisores: [
-      { nome: "João Pedro", foto: "gestao6.jpeg", whatsapp: "5583933333333", cargo: "Supervisor Comercial", tabela: ["Tabela B"] },
-      { nome: "Larissa Melo", foto: "gestao7.jpeg", whatsapp: "5583922222222", cargo: "Supervisor Comercial", tabela: ["Tabela B"] },
-      { nome: "Daniel Nunes", foto: "gestao8.jpeg", whatsapp: "5583911111111", cargo: "Supervisor Comercial", tabela: ["Tabela B"] },
+      { nome: "supervisor1", foto: "gestao6.jpeg", whatsapp: "5583933333333", cargo: "Supervisor Comercial", tabela: ["Tabela B"] },
+      { nome: "supervisor2", foto: "gestao7.jpeg", whatsapp: "5583922222222", cargo: "Supervisor Comercial", tabela: ["Tabela B"] },
+      { nome: "supervisor3", foto: "gestao8.jpeg", whatsapp: "5583911111111", cargo: "Supervisor Comercial", tabela: ["Tabela B"] },
     ]
   },
   C: {
-    gerente: { nome: "Paula Rodrigues", foto: "gestao9.jpeg", whatsapp: "5583900000000", cargo: "Gerente Comercial", tabela: ["Tabela C"] },
+    gerente: { nome: "Tulio", foto: "gestao9.jpeg", whatsapp: "5583900000000", cargo: "Gerente Comercial", tabela: ["Tabela C"] },
     supervisores: [
-      { nome: "Carla Mendes", foto: "gestao10.jpeg", whatsapp: "5583899999999", cargo: "Supervisor Comercial", tabela: ["Tabela C"] },
-      { nome: "Fábio Lima", foto: "gestao11.jpeg", whatsapp: "5583898888888", cargo: "Supervisor Comercial", tabela: ["Tabela C"] },
-      { nome: "Renata Souza", foto: "gestao12.jpeg", whatsapp: "5583897777777", cargo: "Supervisor Comercial", tabela: ["Tabela C"] },
+      { nome: "supersor1", foto: "gestao10.jpeg", whatsapp: "5583899999999", cargo: "Supervisor Comercial", tabela: ["Tabela C"] },
+      { nome: "supervisor2", foto: "gestao11.jpeg", whatsapp: "5583898888888", cargo: "Supervisor Comercial", tabela: ["Tabela C"] },
+      { nome: "supervisor3", foto: "gestao12.jpeg", whatsapp: "5583897777777", cargo: "Supervisor Comercial", tabela: ["Tabela C"] },
     ]
   },
   D: {
-    gerente: { nome: "Everton Silva", foto: "gestao13.jpeg", whatsapp: "5583896666666", cargo: "Gerente Comercial", tabela: ["Tabela D"] },
+    gerente: { nome: "Salezio", foto: "gestao13.jpeg", whatsapp: "5583896666666", cargo: "Gerente Comercial", tabela: ["Tabela D"] },
     supervisores: [
-      { nome: "Bruno Costa", foto: "gestao14.jpeg", whatsapp: "5583895555555", cargo: "Supervisor Comercial", tabela: ["Tabela D"] },
-      { nome: "Mariana Lima", foto: "gestao15.jpeg", whatsapp: "5583894444444", cargo: "Supervisor Comercial", tabela: ["Tabela D"] },
-      { nome: "Gustavo Rocha", foto: "gestao16.jpeg", whatsapp: "5583893333333", cargo: "Supervisor Comercial", tabela: ["Tabela D"] },
+      { nome: "supervisor1", foto: "gestao14.jpeg", whatsapp: "5583895555555", cargo: "Supervisor Comercial", tabela: ["Tabela D"] },
+      { nome: "supervisor2", foto: "gestao15.jpeg", whatsapp: "5583894444444", cargo: "Supervisor Comercial", tabela: ["Tabela D"] },
+      { nome: "supervisor3", foto: "gestao16.jpeg", whatsapp: "5583893333333", cargo: "Supervisor Comercial", tabela: ["Tabela D"] },
     ]
   },
   E: {
-    gerente: { nome: "Bruno Correia", foto: "gestao17.jpeg", whatsapp: "5583892222222", cargo: "Gerente Comercial", tabela: ["Tabela E"] },
+    gerente: { nome: "Sandra", foto: "gestao17.jpeg", whatsapp: "5583892222222", cargo: "Gerente Comercial", tabela: ["Tabela E"] },
     supervisores: [
-      { nome: "Tatiane Melo", foto: "gestao18.jpeg", whatsapp: "5583891111111", cargo: "Supervisor Comercial", tabela: ["Tabela E"] },
-      { nome: "Carlos Nunes", foto: "gestao19.jpeg", whatsapp: "5583890000000", cargo: "Supervisor Comercial", tabela: ["Tabela E"] },
-      { nome: "Larissa Fernandes", foto: "gestao20.jpeg", whatsapp: "5583889999999", cargo: "Supervisor Comercial", tabela: ["Tabela E"] },
+      { nome: "supervisor1", foto: "gestao18.jpeg", whatsapp: "5583891111111", cargo: "Supervisor Comercial", tabela: ["Tabela E"] },
+      { nome: "supervisor2", foto: "gestao19.jpeg", whatsapp: "5583890000000", cargo: "Supervisor Comercial", tabela: ["Tabela E"] },
+      { nome: "supervisor3", foto: "gestao20.jpeg", whatsapp: "5583889999999", cargo: "Supervisor Comercial", tabela: ["Tabela E"] },
     ]
   },
   F: {
-    gerente: { nome: "Maria Rita", foto: "gestao21.jpeg", whatsapp: "5583888888888", cargo: "Gerente Comercial", tabela: ["Tabela F"] },
+    gerente: { nome: "Aline", foto: "gestao21.jpeg", whatsapp: "5583888888888", cargo: "Gerente Comercial", tabela: ["Tabela F"] },
     supervisores: [
-      { nome: "Everthon Lima", foto: "gestao22.jpeg", whatsapp: "5583887777777", cargo: "Supervisor Comercial", tabela: ["Tabela F"] },
-      { nome: "Kayo Rocha", foto: "gestao23.jpeg", whatsapp: "5583886666666", cargo: "Supervisor Comercial", tabela: ["Tabela F"] },
-      { nome: "Carol Mendes", foto: "gestao24.jpeg", whatsapp: "5583885555555", cargo: "Supervisor Comercial", tabela: ["Tabela F"] },
+      { nome: "supervisor1", foto: "gestao22.jpeg", whatsapp: "5583887777777", cargo: "Supervisor Comercial", tabela: ["Tabela F"] },
+      { nome: "supervisor2", foto: "gestao23.jpeg", whatsapp: "5583886666666", cargo: "Supervisor Comercial", tabela: ["Tabela F"] },
+      { nome: "supervisor3", foto: "gestao24.jpeg", whatsapp: "5583885555555", cargo: "Supervisor Comercial", tabela: ["Tabela F"] },
     ]
   }
 };
